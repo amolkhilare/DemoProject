8c802d369f70765f7804d2c8d649f14d6d702243
Amol <amol.khilare@extrapreneursindia.com>
2022-12-28 13:56:34 +0530

    Added sysout A

--- DemoProject/src/TestDemo.java
+++ DemoProject/src/TestDemo.java
@@ -2,8 +2,7 @@
 public class TestDemo {
 	public static void main(String args[]) {
 		
-		System.out.println("Hello World");
-		System.out.println("Hi");
-		//12345678
+		System.out.println("A");
+		System.out.println("");
 	}
 }
